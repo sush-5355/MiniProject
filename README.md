7ca18ef03e70a00263229fc7e550ad432fc2783d
Sushant Suman <sushantsuman.sbg@gmail.com>
2023-08-27 22:44:24 +0530

    paramiko connection test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MiniProject
+
+
+Ye mai hu aur ye mini ka project hai. Kripya kr ke seriously le isko
---
+++ paramiko_config.py
@@ -0,0 +1,28 @@
+import paramiko
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+hostname = os.environ.get("HOSTNAME")
+username = os.environ.get("USERNAME")
+password = os.environ.get("PASSWORD")
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+def connect_to_pc():
+
+    try:
+
+        client.connect(
+        hostname=hostname, username=username, password=password, port=22, timeout=30
+    )
+        return dict(status=True,connectionClient=client)
+    except Exception as e:
+        return dict(status=False,msg=str(e))
+        
+
+def close_connection():
+    client.close()
+    
